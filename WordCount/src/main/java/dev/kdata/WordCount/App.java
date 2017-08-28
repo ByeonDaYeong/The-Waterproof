a0367560a15e5d2bfd1153bfb1d63dca81f1f120
KTJ <justkukaro@naver.com>
2017-08-28 22:03:57 +0900

    test

---
+++ WordCount/src/main/java/dev/kdata/WordCount/App.java
@@ -0,0 +1,13 @@
+package dev.kdata.WordCount;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
