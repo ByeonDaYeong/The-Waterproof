a9b215b91cf2daae9c2283cf3f62ae3302c9a0b8
dl57934 <dl57934@naver.com>
2017-08-30 04:37:38 +0900

    Revert "만약 소리가 울리게된다면 소리(사이렌 소리로 바꿈)가 꺼져있어도 최대 크기로 울리게 설정 지도 api추가 침수 위험이 있다면 자동으로 위도 경도 받아와서 지도에 추가해주면 자기 위치 지도에 나올 것 같음  상태 알림바 설정 완료"
    This reverts commit aa434116de7f375a314d872e33185c4dfba43b32.

--- Music/app/src/main/java/com/example/dl579/singltonesocket/MainActivity.java
+++ Music/app/src/main/java/com/example/dl579/singltonesocket/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.dl579.singltonesocket;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
-import android.media.AudioManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,33 +9,22 @@ import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class MainActivity extends AppCompatActivity {
     Client client = new Client();
-    
-    AudioManager am ;
+    String Message;
+    Button PlayButton;
     Button StopButton;
+    Button SendMsessage;
     Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PlayButton = (Button) findViewById(R.id.PlayButton);
         StopButton = (Button) findViewById(R.id.StopButton);
-        FragmentManager fragmentManager = getFragmentManager();
-        MapFragment mapFragment = (MapFragment)fragmentManager
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        SendMsessage = (Button) findViewById(R.id.SendMessage);
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, 1);
-      am = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
         client.start();
         checkData();
     }
@@ -49,7 +36,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     if (client.message.equals("200")) {
                         SendSMS("01072267934", "이상훈");
                         client.message = "";
-                        am.setStreamVolume(AudioManager.STREAM_MUSIC, 15, AudioManager.FLAG_PLAY_SOUND);
                         intent = new Intent(MainActivity.this,MyService.class);
                         intent.putExtra("PlayOrStop","200");
                         startService(intent);
@@ -62,18 +48,5 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void SendSMS(String number, String msg) {
         SmsManager sms = SmsManager.getDefault();
         sms.sendTextMessage(number, null, msg, null, null);
-    }
-
-    @Override
-    public void onMapReady(final GoogleMap map) {
-        LatLng SEOUL = new LatLng(37.56, 126.97);
-
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(SEOUL);
-        markerOptions.title("서울");
-        markerOptions.snippet("한국의 수도");
-        map.addMarker(markerOptions);
-        map.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
-        map.animateCamera(CameraUpdateFactory.zoomTo(10));
     }
 }
